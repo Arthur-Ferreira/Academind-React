c9ebe4e85d36d72d332bbda3e167eaa9e2fe6045
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-10-30 23:15:44 -0300

    React deep content upload new content

---
+++ 3_React_Deep_Dive/exercises/forwarding_Props/src/Input.jsx
@@ -0,0 +1,5 @@
+export default function Input() {
+  // return a <textarea> if a richText prop is true
+  // return an <input> otherwise
+  // forward / set the received props on the returned elements
+}
